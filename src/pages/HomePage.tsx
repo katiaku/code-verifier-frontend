ed92988f9845429977ccbc11ef5c19cba0438899
Katia <kushnir.ekaterina@gmail.com>
2023-07-21 11:42:31 +0200

    Style Dashboard

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
+import { Dashboard } from '../components/dashboard/Dashboard';
 
 export const HomePage = () => {
     return (
-        <div>
-            <h1>
-                Home Page
-            </h1>
-        </div>
+        <Dashboard />
     )
 }
