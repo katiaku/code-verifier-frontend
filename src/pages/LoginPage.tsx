b4a22cc8c4f26aa3dccf75342000afd3138c1a03
Katia <kushnir.ekaterina@gmail.com>
2023-07-21 12:22:52 +0200

    Add RegisterMaterial

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,9 +4,6 @@ import { LoginMaterial } from "../components/forms/LoginMaterial";
 export const LoginPage = () => {
     return (
         <div>
-            <h1>
-                Login Page
-            </h1>
             <LoginMaterial />
         </div>
     )
